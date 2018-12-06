5b5247064e12da7ae523378b541e06069ddc8e37
ws678 <3190552680@qq.com>
2018-12-06 22:55:29 +0800

    项目第一次commit

---
+++ src/main/java/com/jk/HemafalshParentApplication.java
@@ -0,0 +1,12 @@
+package com.jk;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HemafalshParentApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HemafalshParentApplication.class, args);
+	}
+}
